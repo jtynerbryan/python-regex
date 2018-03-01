c68700fe0b6d9c957af132efe0bacabb852dab45
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-01 11:34:56 -0500

    added alternate way to open, read, and close files when size of file is unknown

--- address_book.py
+++ address_book.py
@@ -9,3 +9,10 @@ last_name = r"Love"
 first_name = r"Kenneth"
 print(re.match(last_name, data))
 print(re.search(first_name, data))
+
+# If you don't know the size of a file, it's better to read it a chunk at a time and close it automatically. The following snippet does that:
+
+# with open("some_file.txt") as open_file:
+#    data = open_file.read()
+
+# The with causes the file to automatically close once the action inside of it finishes. And the action inside, the .read(), will finish when there are no more bytes to read from the file.
