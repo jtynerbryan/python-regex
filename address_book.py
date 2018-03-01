83b341ac829bcfb3a581ab151e84b3865a3faa74
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-01 12:48:40 -0500

    using +, ?, * and {num} for more exact regex searches

--- address_book.py
+++ address_book.py
@@ -7,8 +7,20 @@ names_file.close()
 # use match for begining of string, search for anywhere in string
 last_name = r"Love"
 first_name = r"Kenneth"
-# print(re.match(last_name, data))
-# print(re.search(first_name, data))
+print(re.match(last_name, data))
+print(re.search(first_name, data))
+
+# + for 1-infinite occurences, ? for 0-1 occurences, * for 0-infinite occurences, {num} for how many occurences
+print(re.findall(r'\(?\d{3}\)?-?\s?\d{3}-\d{4}', data))
+
+print(re.findall(r'\w*, \w+', data))
+
+
+
+
+
+
+
 
 # If you don't know the size of a file, it's better to read it a chunk at a time and close it automatically. The following snippet does that:
 
@@ -16,5 +28,3 @@ first_name = r"Kenneth"
 #    data = open_file.read()
 
 # The with causes the file to automatically close once the action inside of it finishes. And the action inside, the .read(), will finish when there are no more bytes to read from the file.
-
-print(re.search(r'\(\d\d\d\) \d\d\d-\d\d\d\d', data))
