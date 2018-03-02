dbb3936af0c0b69a5780eb8bb22ae4f28fcb2f5a
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-02 11:32:29 -0500

    regexing with .compile(), .finditer() and .groupdict()

--- address_book.py
+++ address_book.py
@@ -10,21 +10,21 @@ first_name = r"Kenneth"
 print(re.match(last_name, data))
 print(re.search(first_name, data))
 
-# # # + for 1-infinite occurences, ? for 0-1 occurences, * for 0-infinite occurences, {num} for how many occurences
+# + for 1-infinite occurences, ? for 0-1 occurences, * for 0-infinite occurences, {num} for how many occurences
 print(re.findall(r'\(?\d{3}\)?-?\s?\d{3}-\d{4}', data))
-#
+
 print(re.findall(r'\w*, \w+', data))
-#
-# # find email addresses using sets
+
+# find email addresses using sets
 print(re.findall(r'[-\w\d+.]+@[-\w\d.]+', data))
-#
-# # find all occurences of a specifc word using \b (boundaries) count and IGNORECASE (shorthand='I')
+
+# find all occurences of a specifc word using \b (boundaries) count and IGNORECASE (shorthand='I')
 print(re.findall(r'\b[trehous]{9}\b', data, re.I))
-#
-# # find email addresses not ending in .gov
+
+# find email addresses not ending in .gov
 print(re.findall(r'\b@[\w\d.]*[^gov\t]+', data, re.I))
-#
-# # Use VERBOSE(shorthand='X') flag for multi-line strings
+
+# Use VERBOSE(shorthand='X') flag for multi-line strings
 print(re.findall(r'''
     \b[-\w]+,
     \s
@@ -40,6 +40,22 @@ print(re.findall(r'''
      ([\w\s]+,\s[\w\s.]+)\t? # job and company
      (@[\w\d]+)?$ # Twitter
 ''', data, re.X|re.M))
+
+# using .compile()
+line = re.compile(r'''
+     ^(?P<name>(?P<last>[-\w ]*), \s(?P<first>[-\w ]+))\t # names
+     (?P<email>[-\w\d.+]+@[-\w\d.]+)\t # emails
+     (?P<phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t # phones
+     (?P<job>[\w\s]+,\s[\w\s.]+)\t? # job and company
+     (?P<twitter>@[\w\d]+)?$ # Twitter
+''', re.X|re.M)
+
+print(line.search(data).groupdict())
+
+# using .find_iter() kind of like .findall
+
+for match in line.finditer(data):
+    print('{first} {last} <{email}>'.format(**match.groupdict()))
 
 # If you don't know the size of a file, it's better to read it a chunk at a time and close it automatically. The following snippet does that:
 
